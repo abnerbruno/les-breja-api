6b7f26d0101eacc6593a1a164b643d818e8f3a12
Bruno Abner <bruno.santos@grupoimg.com.br>
2022-04-11 15:43:55 -0300

    insert command Line

--- src/main/java/com/example/brejaapi/BrejaApiApplication.java
+++ src/main/java/com/example/brejaapi/BrejaApiApplication.java
@@ -1,13 +1,25 @@
 package com.example.brejaapi;
 
+import com.example.brejaapi.domain.repository.CervejaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BrejaApiApplication {
+public class BrejaApiApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BrejaApiApplication.class, args);
+	}
+
+	@Autowired
+	CervejaRepository cr;
+
+	@Override
+	public void run(String... args) throws Exception
+	{
+		
 	}
 
 }
